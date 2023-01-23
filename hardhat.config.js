4df014b16082a06d749959fc5833b3b52a784b85
Sneh Aashish Gupta <snehaashishgupta.mnit@gmail.com>
2023-01-23 22:16:00 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/9AnC8xhK29f_VhsyyOHNSvy8C5Nv8qZB",
+      accounts: [
+        "98e19ebcbf6f7638a776c88a16493b3cab64d53330769896040a55ef84c9b579",
+      ],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
